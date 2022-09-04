18deb47ed3b1f69532114d63499a101625289d9d
Eduardo Arias <ars.rossess@gmail.com>
2022-09-04 16:11:30 -0500

    template and router addes, homePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
-
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Error } from './components/errors/Error'
+import { Home } from './components/Templates/Home/Home'
 export const App = () => {
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false
+      }
+    }
+  })
   return (
-    <div>App</div>
+    <QueryClientProvider client={queryClient}>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+          <Route
+            path="*"
+            element={<Error/>}
+          />
+        </Routes>
+      </BrowserRouter>
+    
+      </QueryClientProvider>  
   )
 }
---
+++ src/components/errors/Error.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Error = () => {
+  return (
+    <div><h1>BAD BAD BAD BAD BAD</h1></div>
+  )
+}
